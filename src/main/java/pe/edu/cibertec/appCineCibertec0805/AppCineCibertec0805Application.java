f946cfbb2ab4e07bb15265e3ead490563fb08b17
Diego-coder2525 <SUITE@SUITEBE308-ST18.cibertec.edu.pe>
2023-05-08 19:38:44 -0500

    Commit inicial mamguevo

---
+++ src/main/java/pe/edu/cibertec/appCineCibertec0805/AppCineCibertec0805Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appCineCibertec0805;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCineCibertec0805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCineCibertec0805Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appCineCibertec0805/AppCineCibertec0805ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appCineCibertec0805;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCineCibertec0805ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
